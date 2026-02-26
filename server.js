63f0f1fbf233c9dfbe406fb91a07391a3032f64e
judonangelo <juliadonangeloalvescarneirocar@gmail.com>
2026-02-26 09:04:42 -0300

    criacao do servidor

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+
+const upload = multer({dest:'img/'})
+
+app.use('/img'.express.static('img'));
+
+app.use(express.static('public'));
+
+app.listen(3000, () => cconsole.log("rodando em http://localhost:3000"));
